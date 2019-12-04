16cf9e8266e46fe4030002f6df6ff88cbeae2273
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-12-04 22:02:06 +0100

    ...

--- TurbofanJLink/Turbofan/Project/AllJavaObjInstances/Components/InletInstances.m
+++ TurbofanJLink/Turbofan/Project/AllJavaObjInstances/Components/InletInstances.m
@@ -3,11 +3,15 @@
 BeginPackage["Turbofan`Project`AllJavaObjInstances`Components`InletInstances`"]
 (* Exported symbols added here with SymbolName::usage *)  
 
+NewInletObject::usage="NewInletObject[vars]";
+
 Begin["`Private`"] (* Begin Private Context *) 
 
 	NewInletObject[vars_]:=Module[{inlet},
 	 inlet=JavaNew["com.component.inlet.Inlet", vars];   
 	]
+	
+	
 
 End[] (* End Private Context *)
 
--- TurbofanJLink/Turbofan/Project/AllJavaObjInstances/Turbofan/TurbofanInstances.m
+++ TurbofanJLink/Turbofan/Project/AllJavaObjInstances/Turbofan/TurbofanInstances.m
@@ -1,10 +1,15 @@
 (* Wolfram Language Package *)
 
-BeginPackage["Turbofan`Project`AllJavaObjInstances`Turbofan`TurbofanInstances`"]
+BeginPackage["Turbofan`Project`AllJavaObjInstances`Turbofan`TurbofanInstances`",
+{
+"Turbofan`Project`AllJavaObjInstances`Components`InletInstances`"
+}
+]
 (* Exported symbols added here with SymbolName::usage *)  
 
 Begin["`Private`"] (* Begin Private Context *) 
-
+	
+	(* Istanza a partire dalle istanze già create dei componenti... *)
 	NewTurbofanObject[{inletObj_,fanObj_,compressorObj_,combustorObj_,turbineObj_,mixerObj_,nozzleObj_}]:=Module[{turbofan},
 	 	turbofan=JavaNew[
 	 	    "com.engine.turbofan.assflow.TurbofanAssFlow",
@@ -18,6 +23,12 @@ Begin["`Private`"] (* Begin Private Context *)
 	 	];
 	]
 
+	(* Istanza a partire dalle variabili di base, generando l'istanza di ogni componente... *)
+	NewTurbofanObjectFromVars[var1_,var2_]:=Module[{inlet},
+	 inlet=NewInletObject[var1]; 
+	 (* Altre istanze di altri componenti *)
+	 NewTurbofanObject[inlet, others];   
+	]
 	
 End[] (* End Private Context *)
 
