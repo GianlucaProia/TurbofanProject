856942c81bccae02ecfe2a68444f2ffe4bf6309d
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-12-09 22:09:23 +0100

    ...

--- TurbofanJLink/Turbofan/Project/PureMathFunctions/CycleFunctions.m
+++ TurbofanJLink/Turbofan/Project/PureMathFunctions/CycleFunctions.m
@@ -13,8 +13,17 @@ Begin["`Private`"] (* Begin Private Context *)
 	ToutInlet[Tin_,gamma_,MachIn_]:=Ttot[Tin,gamma,MachIn];
 	PoutInlet[ToutInlet_,Tin_,Pin_,gamma_,epsilonD_]:=epsilonD*((ToutInlet/Tin)^(gamma/(gamma-1)))*Pin;
 	
+	(* FAN *)
+	
 	(* COMPRESSORE *)
 	
+	(* COMBUSTORE *)
+	
+	(* TURBINA *)
+	
+	(* MIXER *)
+	
+	(* UGELLO *)
 	
 
 End[] (* End Private Context *)
