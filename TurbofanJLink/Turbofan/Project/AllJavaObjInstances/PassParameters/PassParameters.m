bcefbd85ca93921c77d509401d5573ccc4783e87
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-12-14 14:58:40 +0100

    ...

--- TurbofanJLink/Turbofan/Project/AllJavaObjInstances/PassParameters/PassParameters.m
+++ TurbofanJLink/Turbofan/Project/AllJavaObjInstances/PassParameters/PassParameters.m
@@ -5,6 +5,21 @@ BeginPackage["Turbofan`Project`AllJavaObjInstances`Passparameters`PassParameters
 
 Begin["`Private`"] (* Begin Private Context *) 
 
+	PassFlowParametersOnetoOne[obj1_,obj2_]:=Module[{},
+	 	obj2@setAirFlowIn[
+	 		obj1@getAirFlowOut[]
+	 	]; 
+	]
+	
+	PassFlowParametersTwotoOne[{obj1_,obj2_},obj3_]:=Module[{airFlow1,airFlow2},
+	 	airFlow1=obj1@getAirFlowOut[];
+	 	airFlow2=obj2@getAirFlowOut[];   
+	    obj3@setAirFlow1[airFlow1];
+	    obj3@setAirFlow2[airFlow2];
+	]
+
+	
+	
 End[] (* End Private Context *)
 
 EndPackage[]
--- TurbofanJLink/Turbofan/Project/AllTurbofanModules/TurbofanCycle.m
+++ TurbofanJLink/Turbofan/Project/AllTurbofanModules/TurbofanCycle.m
@@ -2,16 +2,25 @@
 
 BeginPackage["Turbofan`Project`AllTurbofanModules`TurbofanCycle`",
     {
+    (* PASS PARAMETERS... *)
+    "Turbofan`Project`AllJavaObjInstances`Passparameters`PassParameters`",
+    (* 	COMPONENTI... *)
     "Turbofan`Project`ComponentsModules`Output`FanOutput`",
-    "Turbofan`Project`ComponentsModules`Output`InletOutput`"
+    "Turbofan`Project`ComponentsModules`Output`InletOutput`",
+    "Turbofan`Project`ComponentsModules`Output`CompressorOutput`"
     }]
 (* Exported symbols added here with SymbolName::usage *)  
 
 Begin["`Private`"] (* Begin Private Context *) 
 
-	CalculateTurbofanCycle[turbofanObj_]:=Module[{},
-	 	  CalculateFanOutput[turbofanObj@getFan[]]; 
+	CalculateCycleByComponents[inletObj_,fanObj_,compressorObj_,combustorObj_,turbineObj_,mixerObj_,nozzleObj_]:=Module[{},
+	    CalculateInletOutput[inletObj];
+	    PassFlowParametersOnetoOne[inletObj,fanObj];
+	    CalculateFanOutput[fanObj];
+	    PassFlowParametersOnetoOne[fanObj,compressorObj];
+	    CalculateCompressorOutput[compressorObj];
 	]
+
 
 End[] (* End Private Context *)
 
