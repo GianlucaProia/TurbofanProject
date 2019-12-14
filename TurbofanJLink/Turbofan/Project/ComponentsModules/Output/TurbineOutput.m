71569b96d49810c8ec66dbed40cd1df41310d09e
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-12-14 16:39:21 +0100

    Turbine...

---
+++ TurbofanJLink/Turbofan/Project/ComponentsModules/Output/TurbineOutput.m
@@ -0,0 +1,13 @@
+(* Wolfram Language Package *)
+
+BeginPackage["Turbofan`Project`ComponentsModules`Output`TurbineOutput`",
+{
+"Turbofan`Project`ComponentsModulesImp`Output`TurbineOutputImp`"
+}]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
---
+++ TurbofanJLink/Turbofan/Project/ComponentsModulesImp/Output/TurbineOutImp.m
@@ -0,0 +1,14 @@
+(* Wolfram Language Package *)
+
+BeginPackage["Turbofan`Project`ComponentsModulesImp`Output`TurbineOutputImp`",
+{
+"Turbofan`Project`PureMathFunctions`CycleFunctions`"
+}
+]
+(* Exported symbols added here with SymbolName::usage *)  
+
+Begin["`Private`"] (* Begin Private Context *) 
+
+End[] (* End Private Context *)
+
+EndPackage[]
