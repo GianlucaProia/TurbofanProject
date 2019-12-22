dc051b477586cfc3a3eb98d8c4df4a5a09de6bcc
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-12-22 17:16:32 +0100

    ...

--- TurbofanJLink/Turbofan/Project/ComponentsModules/Output/FanOutput.m
+++ TurbofanJLink/Turbofan/Project/ComponentsModules/Output/FanOutput.m
@@ -6,8 +6,15 @@ BeginPackage["Turbofan`Project`ComponentsModules`Output`FanOutput`",
     }]
 (* Exported symbols added here with SymbolName::usage *)  
 
+
 Begin["`Private`"] (* Begin Private Context *) 
 
+(*
+La funzione calcola la temperatura di uscita dal fan,
+la pressione, il flusso in ingresso ai componenti CalculateFanMassFlowIn[],
+il flusso esterno ai componenti, CalculateFanMassFlowOut[],
+il gamma in uscita, che al momento è lo stesso di quello in ingresso.   
+*)
 
 	CalculateFanOutput[fanObj_]:=Module[{},
 		 CalculateFanTout[fanObj];
