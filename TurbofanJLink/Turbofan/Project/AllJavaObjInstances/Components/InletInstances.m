5390b9744f1c41574221ee6921f5700ce445425f
Gianluca Proia <gianlucaproia1985@gmail.com>
2019-12-22 15:20:12 +0100

    ...

--- TurbofanJLink/Turbofan/Project/AllJavaObjInstances/Components/InletInstances.m
+++ TurbofanJLink/Turbofan/Project/AllJavaObjInstances/Components/InletInstances.m
@@ -8,7 +8,8 @@ NewInletObject::usage="NewInletObject[vars]";
 Begin["`Private`"] (* Begin Private Context *) 
 
 	NewInletObject[vars_]:=Module[{inlet},
-	 inlet=JavaNew["com.component.inlet.Inlet", vars];   
+	 inlet=JavaNew["com.component.inlet.Inlet", vars];
+	 inlet   
 	]
 	
 	
